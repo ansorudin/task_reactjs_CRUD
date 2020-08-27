dc607c4790832cde04d12dcc3697293bd1ed55b2
ansorudin <ahmadansorudin@gmail.com>
2020-08-27 21:06:17 +0700

    tambah modal add data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Header from './component/Header';
 import MainMenu from './pages/MainMenu';
 
+
 function App() {
   return (
     <div>
       <Header />
       <MainMenu />
+      
     </div>
   );
 }
--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Axios from 'axios'
+import {Modal} from 'react-bootstrap'
+
+
 
 var linkApi = 'http://localhost:2000/product'
 
 class MainMenu extends React.Component{
 
     state = {
-        data : null
+        data : null,
+        showForm : false
     }
 
     componentDidMount(){
@@ -34,7 +38,7 @@ class MainMenu extends React.Component{
                         <div className="card-body">
                             <h5 className='card-title'>{val.productName}</h5>
                             <h6 className='card-text'>Rp. {val.price}</h6>
-                            <p className='card-text'>Stock : {val.Stock}</p>
+                            <p className='card-text'>Stock : {val.stock}</p>
                             <div className="row justify-content-center"><input type="button" value="Edit Data" className='btn btn-outline-secondary col-10'/></div>
                             <div className="row justify-content-center"><input type="button" value="Delete Data" className='btn btn-outline-secondary mt-2 col-10'/></div>
                         </div>
@@ -44,26 +48,70 @@ class MainMenu extends React.Component{
             )
         })
     }
+    
+    handleModal = () =>{
+        this.setState({showForm : !this.state.showForm})
+    }
+
+    saveBtnHandle = () => {
+        // ambil semua inputan dari user
+       
+        // jika inputan ada semua
+       
+    }
 
     render(){
         if(this.state.data !== null){
             return(
                 <div className='container border border-primary'>
-                    <input type="button" value="Add New Product " className='btn btn-outline-secondary mt-3'/>
+                    <input type="button" value="Add New Product " onClick={() => {this.handleModal()}} className='btn btn-outline-secondary mt-3'/>
                    <div className="row container-fluid">
                        {this.mapData()}
                    </div>
+                   
+                    <Modal size="lg" show={this.state.showForm} onHide={() => this.handleModal()}>
+                        <Modal.Header closeButton>Modal Head Part</Modal.Header>
+                            <Modal.Body>
+                                <form>
+                                    <input type="text" placeholder='Enter New Product Name' className='form-control'/>
+                                    <input type="text" placeholder='Enter New Price' className='form-control'/>
+                                    <input type="text" placeholder='Enter New Stock Qty' className='form-control'/>
+                                    <input type="text" placeholder='Enter Url Image' className='form-control'/>
+                                </form>
+                            </Modal.Body>
+                        <Modal.Footer>
+                            <input type="button" value="Cancel" className='btn btn-outline-secondary' onClick={() => {this.handleModal()}}/>
+                            <input type="button" value="Save" className='btn btn-outline-secondary' onSubmit={this.saveBtnHandle}/>
+
+                        </Modal.Footer>
+                    </Modal>
+                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 </div>
             )
         }
         return(
             <div className='container border border-primary'>
                 <input type="button" value="Add New Product " className='btn btn-outline-secondary mt-3'/>
-                <div class="d-flex justify-content-center">
-                    <div class="spinner-border" role="status">
-                        <span class="sr-only">Loading...</span>
-                    </div>
-                </div>
+               
             </div>
         )
     }
